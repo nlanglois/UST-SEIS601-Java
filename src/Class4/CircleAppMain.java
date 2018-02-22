e88f6de19a33c4e606b4c8ac20d8ac0b27d40ba0
Nikolai Langlois <nlanglois@mac.com>
2018-02-21 19:56:05 -0600

    Completed tasks required in CircleAppMainˆ

--- src/Class4/CircleAppMain.java
+++ src/Class4/CircleAppMain.java
@@ -6,12 +6,21 @@ public class CircleAppMain{
          /*To-Do:
           *Using "new" to instantiate a Circle object and storing its address in a reference variable called "c1" 
           */    	     	
-         
+
+         Circle c1 = new Circle();
+
          /*
           * To-Do: 
           * 1. use the method "findArea" to find the area of circle c1 and store the area in a double variable called myCircleArea
           * 2. use the method "findCircumference" to find the circumference of circle c1 and store the circumference in a double variable called myCircleCircumference
-          * */    	 
+          * */
+
+         double myCircleArea = c1.findArea();
+
+         double myCircleCircumference = c1.findCircumference();
+
+
+
     	 String circle_id = c1.getClass().getName() + "@" + Integer.toHexString(System.identityHashCode(c1));
     	 System.out.println("Area of my circle is: "+ myCircleArea);
     	 System.out.println("Circumference of my circle is: "+ myCircleCircumference);
